bfcabf53df8f7acc12f0859c8ecc5b5ef4e70a9a
Markus <nummimarkus@gmail.com>
2023-05-07 15:11:04 +0300

    10_fibonacci

--- fundamentals/javascript-exercises/10_fibonacci/fibonacci.js
+++ fundamentals/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    var n = Number(num);
+    if(n < 0)
+        return "OOPS";
+    if(n=== 1 || n === 2)
+        return 1;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
