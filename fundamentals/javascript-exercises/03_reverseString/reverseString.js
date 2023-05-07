71aebf3848ffae8ec6f325affdbb16e2293dc4ce
Markus <nummimarkus@gmail.com>
2023-05-07 11:53:02 +0300

    03_reverseString

--- fundamentals/javascript-exercises/03_reverseString/reverseString.js
+++ fundamentals/javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    var s = "" + string;
+    var length = s.length;
+    var reversedString = "";
+    for(var i = length; i > 0; i--)
+    {
+        reversedString += s.charAt(i - 1);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
