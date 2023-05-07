5793ae67ea9c0a4b3773132c08e824002c677d9a
Markus <nummimarkus@gmail.com>
2023-05-07 12:59:37 +0300

    07_tempConversion

--- fundamentals/javascript-exercises/07_tempConversion/tempConversion.js
+++ fundamentals/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  var f = Math.round((fahrenheit -32) * (5/9) * 10) / 10;
+
+  return f;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+  var c = Math.round((celsius * (9/5) + 32) * 10) / 10;
+
+  return c;
 };
 
 // Do not edit below this line
