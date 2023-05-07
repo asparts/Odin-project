1ecf2fc13d5996fbc6cb6487a1f737378a5d50fa
Markus <nummimarkus@gmail.com>
2023-05-07 11:47:29 +0300

    02_repeatString

--- fundamentals/javascript-exercises/02_repeatString/repeatString.js
+++ fundamentals/javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
 
+    var s = "";
+
+    if(times < 0)
+        return "ERROR";
+    for(var i = 0; i < times; i++)
+    {
+        s += string;
+    }
+    return s;
 };
 
 // Do not edit below this line
