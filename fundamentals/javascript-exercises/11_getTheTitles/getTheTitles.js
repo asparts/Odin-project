8deae309e4f42e20c9b7b12e9ea0cbc8fd26c5e2
Markus <nummimarkus@gmail.com>
2023-05-07 15:16:36 +0300

    11_getTheTitles

--- fundamentals/javascript-exercises/11_getTheTitles/getTheTitles.js
+++ fundamentals/javascript-exercises/11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
 
+    var arr = Array.from(array);
+    var s = [];
+    arr.forEach((title) => {
+        s.push(title.title);
+    }); 
+    
+    return s;
 };
 
 // Do not edit below this line
