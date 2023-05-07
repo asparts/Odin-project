eb4108d321ac7f44348e86f88c213b1e6f62b524
Markus <nummimarkus@gmail.com>
2023-05-07 12:08:22 +0300

    04_removeFromArray

--- fundamentals/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ fundamentals/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    var length = args.length;
+    var arr = Array.from(array);
+    for(var i = 0; i < length; i++)
+    {
+        if(arr.includes(args[i]))
+            arr.splice(arr.indexOf(args[i]), 1);
+    }
+return arr;
 
 };
 
