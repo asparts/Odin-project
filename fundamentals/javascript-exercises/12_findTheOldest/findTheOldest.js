df926e545548e101833e973873f673e2879b177f
Markus <nummimarkus@gmail.com>
2023-05-07 16:18:59 +0300

    12_findTheOldest

--- fundamentals/javascript-exercises/12_findTheOldest/findTheOldest.js
+++ fundamentals/javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    var arr = Array.from(people);
+    
+    var s = [];
 
+    arr.forEach((person) => {
+        var obj = {};
+        obj["name"] = person.name;
+        obj["age"] = (person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : new Date().getFullYear() - person.yearOfBirth)
+        s.push(obj);
+    }); 
+    s = s.sort((a, b) => parseInt(b.age) - parseInt(a.age));
+    
+    //return s[0]["name"];
+    var o = Object.values(s[0])[0];
+    var obj = {name: o}
+    return obj;
 };
 
 // Do not edit below this line
