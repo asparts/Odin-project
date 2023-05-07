5b13fe10bad5ccd6a6d43c1aff125a0990292acb
Markus <nummimarkus@gmail.com>
2023-05-07 14:55:40 +0300

    09_palindromes

--- fundamentals/javascript-exercises/09_palindromes/palindromes.js
+++ fundamentals/javascript-exercises/09_palindromes/palindromes.js
@@ -1,4 +1,34 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    var s = "" + string;
+    var ss = ""+string;
+    var length = s.length;
+    var reversedString = "";
+    for(var i = length; i > 0; i--)
+    {
+        reversedString += s.charAt(i - 1);
+    }
+    
+    s = s.toLocaleLowerCase();
+    s = s.replace(/\s/g, ""); // removing space
+    s = s.split('.').join('');
+    s = s.split(',').join('');
+    s = s.split('!').join('');
+    reversedString = reversedString.toLocaleLowerCase();
+    reversedString = reversedString.replace(/\s/g, "");
+    reversedString = reversedString.split(',').join('');
+    reversedString = reversedString.split('.').join('');
+    reversedString = reversedString.split('!').join('');
+
+    for(var i = 0; i < s.length; i++)
+    {
+        if(!(s.charAt(i).match(reversedString.charAt(i))))
+        {
+            return false;
+        }
+    }
+
+        return true;
 
 };
 
